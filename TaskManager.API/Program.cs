a2085e21b7f217e0ad8c060abb51cb7fd4f72803
josefelipesouza <felipecomp023@gmail.com>
2026-04-28 11:05:18 -0300

    correções

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -42,11 +42,15 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
     });
-});    
+});   
 
 var app = builder.Build();
 
